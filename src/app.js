61638bb782e93e2e9dcbf3ad55d895074f22cfca
Jc <jean.christophe.molhant@gmail.com>
2021-10-01 23:50:38 +0200

    feature completed

--- src/app.js
+++ src/app.js
@@ -1,12 +1,7 @@
-import { theme } from './modules/theme.js';
-
-let body = document.querySelector('body');
-let themeSwitch = document.querySelector('.themeSwitch');
-
-themeSwitch.addEventListener('click', (e) => {
-    let themeIndex = parseInt(body.classList[0].slice(5));
-    body.className="";
-    if (themeIndex == 3) themeIndex = 1;
-    else themeIndex++;
-    body.classList.add(`theme${themeIndex}`);
-})
+import * as themeModule from './modules/theme.js';
+let input = document.querySelector('#volume');
+themeModule.initialize();
+input.addEventListener('change', (e) => {
+    themeModule.setTheme(e.target.value);
+    themeModule.setLocalStorage(e.target.value);
+});
--- src/modules/theme.js
+++ src/modules/theme.js
@@ -1,6 +1,28 @@
-const theme = () => {
+const initialize = () => {
+    let locStorageDarkmode = localStorage.getItem('darkmode');
+    let input = document.querySelector('#volume');
+    let returnMessage = `Theme Module Initialized. `;
+    if (!locStorageDarkmode) {
+        setTheme(0);
+        input.value = 0;
+        let add = returnMessage.concat(`First time -> Defaulted to theme 1`);
+        return console.log(add);
+    } else {
+        setTheme(locStorageDarkmode);
+        input.value = locStorageDarkmode;
+        let add = returnMessage.concat(`LocalStorage found and set to theme ${parseInt(locStorageDarkmode) + 1}`);
+        return console.log(add);
+    }
+}
+const setTheme = (theme) => {
     let body = document.querySelector('body');
-    body.style.backgroundColor = "yellow";
+    body.className="";
+    body.classList.add(`theme${parseInt(theme)+1}`);
+    return;
+}
+const setLocalStorage = (theme) => {
+    localStorage.setItem('darkmode', theme);
+    return console.log(`Set LocalStorage to ${parseInt(theme) + 1}`);
 }
 
-export { theme };
+export { setTheme, initialize, setLocalStorage };
