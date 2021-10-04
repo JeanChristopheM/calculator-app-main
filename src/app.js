0672406cc439387c97bf590c922bf8f2b397d6ba
Jean-Christophe Molhant <jean.christophe.molhant@gmail.com>
2021-10-04 14:21:58 +0200

    finished the brain. I think

--- src/app.js
+++ src/app.js
@@ -5,3 +5,13 @@ input.addEventListener('change', (e) => {
     themeModule.setTheme(e.target.value);
     themeModule.setLocalStorage(e.target.value);
 });
+
+import * as calcModule from './modules/calc.js';
+calcModule.initialize();
+let keypad = document.querySelector('.main__keypad');
+keypad.addEventListener('click', (e)=> {
+    if (e.target.nodeName === "BUTTON") {
+        if (e.target.classList.contains('num')) calcModule.keyPressed(parseInt(e.target.textContent));
+        else calcModule.keyPressed(e.target.textContent);
+    }
+});
---
+++ src/modules/calc.js
@@ -0,0 +1,64 @@
+let firstInstruction;
+let secondInstruction;
+let operator;
+
+const initialize = () => {
+    let screen = document.querySelector('.main__screen');
+    return screen.textContent= '0';
+}
+
+const keyPressed = (keyInput) => {
+    let screen = document.querySelector('.main__screen');
+    let currentValue = screen.textContent;
+    if (typeof keyInput == "number") {
+        if (currentValue === '0') {
+            newValue = keyInput;
+        } else newValue = currentValue.concat(keyInput.toString());
+    } else {
+        switch (keyInput) {
+            case "DEL":
+                newValue = currentValue.slice(0,-1);
+                break;
+            case "RESET":
+                newValue = 0;
+                break;
+            case "=":
+                newValue = makeMath(parseFloat(firstInstruction), parseFloat(currentValue), operator);
+                break;
+            case ".":
+                if (!currentValue.includes('.')) newValue = currentValue.concat(keyInput.toString());
+                break;
+            default:
+                firstInstruction = currentValue;
+                operator = keyInput;
+                newValue = 0;
+        }
+    }
+    return refreshScreen(newValue);
+}
+
+const makeMath = (origin, value, operator) => {
+    let result;
+    switch (operator) {
+        case "+": 
+            result = origin + value;
+            break;
+        case "-":
+            result = origin - value;
+            break;
+        case "/":
+            result = origin / value;
+            break;
+        case "x":
+            result = origin * value;
+    }
+    return result;
+}
+
+const refreshScreen = (value) => {
+    let screen = document.querySelector('.main__screen');
+    
+    if (newValue === "") newValue = 0;
+    return screen.textContent = newValue;
+}
+export { initialize, refreshScreen, keyPressed};
